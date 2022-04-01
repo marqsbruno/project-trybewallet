203a4084832672e6e15e2dc5b8025edbe2d10fb1
bruno <marqsbruno@gmail.com>
2022-04-01 15:30:27 -0300

    req 3 tenho que arrumar

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field"> 0 </p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = ({
+  email: PropTypes.string,
+}).isRequire;
+
+export default connect(mapStateToProps)(Header);
